c6ca9a646bd176100283e804111e789edc6ff7aa
Aravind R Pillai <aravindrpillai1992@gmail.com>
2024-08-13 21:02:22 -0400

    URL fix

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -1,8 +1,6 @@
 let BASE_URL = "http://localhost:8000"
 
 const URLS = {
-    //STAGING_CONVERSATION_URL : BASE_URL.concat("/mediatorai/stage/conversation/fileupload"),
-    //STAGING_FILE_UPLOAD_URL : BASE_URL.concat("/mediatorai/stage/conversation/fileupload"), //append conversation_id to the end
     
     STAGING_CONVERSATION_URL : "/mediatorai/stage/conversation/",
     STAGING_FILE_UPLOAD_URL : "/mediatorai/stage/conversation/fileupload/", //append conversation_id to the end
